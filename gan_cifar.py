d2f96e5ab5d66b8fe69e43976f0c0a9bd67d8dd2
Lornatang <shiyipaisizuo@163.com>
2019-04-17 23:11:32 +0800

    Update gan_cifar.py

--- gan_cifar.py
+++ gan_cifar.py
@@ -88,20 +88,19 @@ class Generator(nn.Module):
     self.ngpu = gpus
     self.main = nn.Sequential(
       # inputs is Z, going into a convolution
-      nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
-      nn.BatchNorm2d(ngf * 8),
-      nn.ReLU(True),
-      # state size. (ngf*4) x 4 x 4
-      nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+      nn.ConvTranspose2d(nz, ngf * 4, 4, 1, 0, bias=False),
       nn.BatchNorm2d(ngf * 4),
       nn.ReLU(True),
-      # state size. (ngf*2) x 8 x 8
+      # state size. (ngf*4) x 4 x 4
       nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ngf * 2),
       nn.ReLU(True),
-      # state size. (ngf) x 16 x 16
+      # state size. (ngf*2) x 8 x 8
       nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
       nn.BatchNorm2d(ngf),
+      nn.ReLU(True),
+      # state size. (ngf) x 16 x 16
+      nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
       nn.Tanh()
       # state size. (ngf) x 32 x 32
     )
