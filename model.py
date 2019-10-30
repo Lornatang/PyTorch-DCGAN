81ede0e51f8a4766d4e0bbde73237e1d69580277
Changyu Liu <liuchangyu1111@gmail.com>
2019-10-30 15:02:22 +0800

    Simplified the structure of the model file

--- model.py
+++ model.py
@@ -21,13 +21,6 @@ to tell real images apart from fakes.
 
 import torch.nn as nn
 
-
-nz = 100
-ngf = 64
-ndf = 64
-nc = 3
-
-
 # custom weights initialization called on netG and netD
 def weights_init(m):
     classname = m.__class__.__name__
@@ -39,70 +32,81 @@ def weights_init(m):
 
 
 class Generator(nn.Module):
-    def __init__(self, ngpu):
+    def __init__(self):
         super(Generator, self).__init__()
-        self.ngpu = ngpu
-
+        
         self.main = nn.Sequential(
             # inputs is Z, going into a convolution
-            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 8),
+            nn.ConvTranspose2d(100, 64 * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(64 * 8),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4),
+            nn.ConvTranspose2d(64 * 8, 64 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 4),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2),
+            nn.ConvTranspose2d(64 * 4, 64 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf),
+            nn.ConvTranspose2d(64 * 2, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
             nn.ReLU(True),
             # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d(ngf, nc, 5, 3, 1, bias=False),
+            nn.ConvTranspose2d(64, 3, 5, 3, 1, bias=False),
             nn.Tanh()
             # state size. (nc) x 96 x 96
         )
 
     def forward(self, inputs):
-        if inputs.is_cuda and self.ngpu > 1:
-            outputs = nn.parallel.data_parallel(self.main, inputs, range(self.ngpu))
-        else:
-            outputs = self.main(inputs)
-        return outputs
+      """ forward layer
+      Args:
+        inputs: input tensor data.
+      Returns:
+        forwarded data.
+      """
+      if torch.cuda.is_available():
+        outputs = nn.parallel.data_parallel(self.main, inputs)
+      else:
+        outputs = self.main(inputs)
+      return outputs
 
 
 class Discriminator(nn.Module):
-    def __init__(self, ngpu):
+    def __init__(self):
         super(Discriminator, self).__init__()
-        self.ngpu = ngpu
 
         self.main = nn.Sequential(
             # inputs is (nc) x 64 x 64
-            nn.Conv2d(nc, ndf, 5, 3, 1, bias=False),
+            nn.Conv2d(3, 64, 5, 3, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
+            nn.Conv2d(64, 64 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 2),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
+            nn.Conv2d(64 * 2, 64 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 4),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
+            nn.Conv2d(64 * 4, 64 * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 8),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Conv2d(64 * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
 
     def forward(self, inputs):
-        if inputs.is_cuda and self.ngpu > 1:
-            outputs = nn.parallel.data_parallel(self.main, inputs, range(self.ngpu))
-        else:
-            outputs = self.main(inputs)
-        return outputs
+      """ forward layer
+      Args:
+        inputs: input tensor data.
+      Returns:
+        forwarded data.
+      """
+      inputs = inputs.view(inputs.size(0), -1)
+      if torch.cuda.is_available():
+        outputs = nn.parallel.data_parallel(self.main, inputs)
+      else:
+        outputs = self.main(inputs)
+      return outputs
